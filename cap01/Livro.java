5b695419f74b77cd6c2357d4ea8aa6d1767bc4b1
Abner.Lima <abnerlima.1997@gmail.com>
2020-11-08 19:36:08 -0300

    JAVA 8 - LINGUAGEM E PLATAFORMA

---
+++ cap01/Livro.java
@@ -0,0 +1,8 @@
+package cap01;
+public class Livro {
+    public static void main(String[] args) {
+        System.out.println("JAVA 8");
+        System.out.println("Ensino didático");
+        System.out.println("Sérgio Furgei");
+    }
+}
